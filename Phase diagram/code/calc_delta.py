c12ca095f966dcdc5002bfd2477855e559c521ef
XuhaoWan <73831094+XuhaoWan@users.noreply.github.com>
2023-03-07 11:27:07 +0800

    Update calc_delta.py

--- Phase diagram/code/calc_delta.py
+++ Phase diagram/code/calc_delta.py
@@ -23,19 +23,18 @@ Ti = einHEA('Ti', 0, 136)
 In = einHEA('In', 0, 144)
 Rh = einHEA('Rh', 0, 135)
 Sn = einHEA('Sn', 27, 141)
-Pt = einHEA('Pt', 0, 128)
 Pd = einHEA('Pd', 27, 131)
 Ru = einHEA('Ru', 0, 126)
 Au = einHEA('Au', 0, 144)
 Zn = einHEA('Zn', 0, 131)
 
 N_all = Ga.number + Fe.number + Mn.number + Cu.number + Ni.number + Ti.number + In.number + Rh.number + Sn.number + \
-        Pt.number + Pd.number + Ru.number + Au.number + Zn.number
+        Pd.number + Ru.number + Au.number + Zn.number
 print(N_all)
 
 
 R_ave = (Ga.number*Ga.radius + Fe.number*Fe.radius + Mn.number*Mn.radius + Cu.number*Cu.radius + Ni.number*Ni.radius + \
-        Ti.number*Ti.radius + In.number*In.radius + Rh.number*Rh.radius + Sn.number*Sn.radius + Pt.number*Pt.radius +
+        Ti.number*Ti.radius + In.number*In.radius + Rh.number*Rh.radius + Sn.number*Sn.radius  +
          Pd.number*Pd.radius + Ru.number*Ru.radius + Au.number*Au.radius + Zn.number*Zn.radius)/N_all
 print(R_ave)
 
@@ -43,7 +42,7 @@ N = Ga.number*math.pow((1-Ga.radius/R_ave), 2) + Fe.number*math.pow((1-Fe.radius
     Mn.number*math.pow((1-Mn.radius/R_ave), 2) + Cu.number*math.pow((1-Cu.radius/R_ave), 2) + \
     Ni.number*math.pow((1-Ni.radius/R_ave), 2) + Ti.number*math.pow((1-Ti.radius/R_ave), 2) + \
     In.number*math.pow((1-In.radius/R_ave), 2) + Rh.number*math.pow((1-Rh.radius/R_ave), 2) + \
-    Sn.number*math.pow((1-Sn.radius/R_ave), 2) + Pt.number*math.pow((1-Pt.radius/R_ave), 2) + \
+    Sn.number*math.pow((1-Sn.radius/R_ave), 2)  + \
     Pd.number*math.pow((1-Pd.radius/R_ave), 2) + Ru.number*math.pow((1-Ru.radius/R_ave), 2) + \
     Au.number*math.pow((1-Au.radius/R_ave), 2) + Zn.number*math.pow((1-Zn.radius/R_ave), 2)
 
